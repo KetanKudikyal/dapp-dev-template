03153941b817df3aedee5893f399eacf4dfeb785
Ketan <krotusppp@gmail.com>
2023-06-08 15:49:22 +0530

    fix: add expand icon

--- src/pages/graphs.tsx
+++ src/pages/graphs.tsx
@@ -92,9 +92,26 @@ const Graphs = () => {
             }}
             className='h-[60%] rounded-xl border border-[#2A3136] bg-[#121619] p-4'
           >
-            <p className='mb-2 text-[18px]  font-bold text-[#c7c7cc]'>
-              Floor price
-            </p>
+            <div className='flex items-center justify-between'>
+              <p className='mb-2 text-[18px]  font-bold text-[#c7c7cc]'>
+                Floor price
+              </p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='-mt-1 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </div>
+
             <div
               className='h-[300px]
             '
@@ -116,7 +133,23 @@ const Graphs = () => {
             }}
             className='relative mt-4 h-[40%] rounded-xl border border-[#2A3136] bg-[#121619] p-4'
           >
-            <p className='mb-2 text-[18px] font-bold text-[#c7c7cc]'>Sales</p>
+            <Row isBetween>
+              <p className='mb-2 text-[18px] font-bold text-[#c7c7cc]'>Sales</p>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='-mt-1 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Row>
             <div className='h-[100px] '>
               <ScatterChart height={180} />
             </div>
@@ -156,12 +189,31 @@ const Graphs = () => {
             }}
             className='h-[25%] w-full rounded-xl border border-[#2A3136] bg-[#121619] p-2'
           >
-            <div className='text-[14px] font-normal text-[#c7c7cc]'>
-              Unique owners
-            </div>
-            <div className='text-[12px]'>
-              5,637 <span className='text-[#c7c7cc]'>(56.37% unique)</span>
-            </div>
+            <Row isBetween>
+              <div>
+                <div className='text-[14px] font-normal text-[#c7c7cc]'>
+                  Unique owners
+                </div>
+                <div className='text-[12px]'>
+                  5,637 <span className='text-[#c7c7cc]'>(56.37% unique)</span>
+                </div>
+              </div>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='-mt-2 mr-2 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Row>
+
             <div className='h-[70px] overflow-hidden'>
               <Graph
                 address='0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
@@ -185,7 +237,27 @@ const Graphs = () => {
             }}
             className='h-[25%] w-full rounded-xl border border-[#2A3136] bg-[#121619] p-2'
           >
-            <p className='text-[14px] font-normal text-[#c7c7cc]'>Sentiment</p>
+            <Row isBetween isCentered>
+              <div>
+                <div className='text-[14px] font-normal text-[#c7c7cc]'>
+                  Sentiment
+                </div>
+              </div>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='mr-2 mt-1 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Row>
             <StackedBarChart height={80} pointWidth={70} showAxis={false} />
           </div>
           <div
@@ -204,13 +276,31 @@ const Graphs = () => {
             }}
             className='h-[25%] w-full rounded-xl border border-[#2A3136] bg-[#121619] p-2'
           >
-            <div className='text-[14px] font-normal text-[#c7c7cc]'>
-              Listed tokens
-            </div>
-            <div className='text-[12px]'>
-              338
-              <span className='text-[#c7c7cc]'>/10,000 (3.38%)</span>
-            </div>
+            <Row isBetween>
+              <div>
+                <div className='text-[14px] font-normal text-[#c7c7cc]'>
+                  Listed tokens
+                </div>
+                <div className='text-[12px]'>
+                  338
+                  <span className='text-[#c7c7cc]'>/10,000 (3.38%)</span>
+                </div>
+              </div>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='-mt-2 mr-2 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Row>
             <div className='h-[70px] overflow-hidden'>
               <Graph
                 address='0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
@@ -228,9 +318,27 @@ const Graphs = () => {
             }}
             className='h-[25%] w-full rounded-xl border border-[#2A3136] bg-[#121619] p-2'
           >
-            <p className='mb-2 text-[14px] font-normal text-[#c7c7cc]'>
-              Listing Depth
-            </p>
+            <Row isBetween isCentered className='mb-3'>
+              <div>
+                <div className='text-[14px] font-normal text-[#c7c7cc]'>
+                  Listing Depth
+                </div>
+              </div>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 24 24'
+                strokeWidth={1.5}
+                stroke='currentColor'
+                className='mr-2 mt-1 h-5 w-5'
+              >
+                <path
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  d='M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15'
+                />
+              </svg>
+            </Row>
             <BarChart height={70} showAxis={false} />
           </div>
         </div>
